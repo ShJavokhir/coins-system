f2a86145a66e31264350d40851e97de0c4d6ed85
Zafarbek1810 <zafarbekyoldoshev1810@gmail.com>
2023-03-07 18:28:31 +0500

    fix

--- src/Components/Pages/Dashboard/CEO/Product/index.jsx
+++ src/Components/Pages/Dashboard/CEO/Product/index.jsx
@@ -1,8 +1,5 @@
-import { InboxOutlined, UploadOutlined } from "@mui/icons-material";
-import { Button, Tabs, Upload } from "antd";
-import Dragger from "antd/es/upload/Dragger";
+import { Tabs } from "antd";
 import React from "react";
-import { toast } from "react-toastify";
 import DashboardLayout from "../../../../Layout";
 
 const Product = () => {
@@ -29,37 +26,12 @@ const Product = () => {
 };
 
 const ProductAdd = () => {
-    const props = {
-        name: 'file',
-        multiple: true,
-        action: 'https://www.mocky.io/v2/5cc8019d300000980a055e76',
-        onChange(info) {
-          const { status } = info.file;
-          if (status !== 'uploading') {
-            console.log(info.file, info.fileList);
-          }
-          if (status === 'done') {
-            toast.success(`${info.file.name} file uploaded successfully.`);
-          } else if (status === 'error') {
-            toast.error(`${info.file.name} file upload failed.`);
-          }
-        },
-        onDrop(e) {
-          console.log('Dropped files', e.dataTransfer.files);
-        },
-      };
+    const handle=()=>{
+      console.log();
+    }
   return (
     <>
-      <Dragger {...props}>
-    <p className="ant-upload-drag-icon">
-      <InboxOutlined />
-    </p>
-    <p className="ant-upload-text">Click or drag file to this area to upload</p>
-    <p className="ant-upload-hint">
-      Support for a single or bulk upload. Strictly prohibit from uploading company data or other
-      band files
-    </p>
-  </Dragger>
+      <input type="file" onClick={handle}/>
     </>
   );
 };
